e927f23e56f3d37e3badbf5c06d92c0b66764770
kumkum03-hub <kumkumkamthan2003@gmail.com>
2026-02-11 17:56:51 +0530

    Update 3pieceSet.jsx

--- src/Pages/Categories/MenCategory/EthnicWear/3pieceSet.jsx
+++ src/Pages/Categories/MenCategory/EthnicWear/3pieceSet.jsx
@@ -1,5 +1,5 @@
-import Navbar from '../../../../components/Navbar';
-import Footer from '../../../../components/Footer';
+import Navbar from '../../../../Components/Navbar';
+import Footer from '../../../../Components/Footer';
 import './3pieceSet.css';
 import { useRef, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
